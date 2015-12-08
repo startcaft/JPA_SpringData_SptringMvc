622b9c6cd15889439720b8eb3ff3800d49d1d84c
startcaft <51418996@qq.com>
2015-12-08 20:56:39 +0800

    mybatis学习之简单的商城数据模型

---
+++ mybatis_shop/src/main/java/mybaitsi/shop/model/Orders.java
@@ -0,0 +1,66 @@
+package mybaitsi.shop.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Orders {
+	
+	private int id;
+	private Date buy_date;
+	private Date pay_date;
+	private Date confirm_date;
+	private int status;
+	private User user;
+	private Address address;
+	
+	private List<Goods> goodses;//多对多的关系-单向
+	
+	public List<Goods> getGoodses() {
+		return goodses;
+	}
+	public void setGoodses(List<Goods> goodses) {
+		this.goodses = goodses;
+	}
+	public Date getPay_date() {
+		return pay_date;
+	}
+	public void setPay_date(Date pay_date) {
+		this.pay_date = pay_date;
+	}
+	public Date getConfirm_date() {
+		return confirm_date;
+	}
+	public void setConfirm_date(Date confirm_date) {
+		this.confirm_date = confirm_date;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getBuy_date() {
+		return buy_date;
+	}
+	public void setBuy_date(Date buy_date) {
+		this.buy_date = buy_date;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
---
+++ mybatis_shop/src/main/java/mybaitsi/shop/model/ShopCat.java
@@ -0,0 +1,15 @@
+package mybaitsi.shop.model;
+
+import java.util.List;
+
+public class ShopCat {
+	
+	private List<Goods> goodses;
+
+	public List<Goods> getGoodses() {
+		return goodses;
+	}
+	public void setGoodses(List<Goods> goodses) {
+		this.goodses = goodses;
+	}
+}
