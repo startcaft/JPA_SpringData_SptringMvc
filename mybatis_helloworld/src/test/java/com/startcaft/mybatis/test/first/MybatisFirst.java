0c7feb3f8919e8c63a60da16cab055730994c48e
startcaft <51418996@qq.com>
2015-12-13 21:38:27 +0800

    映射文件中注意#{}和${}的区别。#{}标示占位符，${}标示拼接SQL串； #{}如果是简单类型，里面的名称随意； ${}如果是简单类型，里面的名称只能是value；

--- mybatis_helloworld/src/test/java/com/startcaft/mybatis/test/first/MybatisFirst.java
+++ mybatis_helloworld/src/test/java/com/startcaft/mybatis/test/first/MybatisFirst.java
@@ -1,8 +1,8 @@
 package com.startcaft.mybatis.test.first;
 
-
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -13,39 +13,59 @@ import org.junit.Test;
 import com.startcaft.mybatis.po.User;
 
 public class MybatisFirst {
-	
-	//根据id，得到一个User对象
+
+	// 根据id，得到一个User对象
 	@Test
 	public void findUserByIdTest() throws IOException {
-		
-		
+
 		SqlSession session = null;
 		try {
-			//加载mybatis基础配置文件
+			// 加载mybatis基础配置文件
 			InputStream inputSream = Resources.getResourceAsStream("mybatis_config.xml");
-			
-			//创建SqlSessionFactory会话工厂，需要传入mybatis的配置信息
+
+			// 创建SqlSessionFactory会话工厂，需要传入mybatis的配置信息
 			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputSream);
-			
-			//通过会话工厂创建SqlSession对象
+
+			// 通过会话工厂创建SqlSession对象
 			session = factory.openSession();
-			
-			//通过SqlSession对象操作数据库
-			//selectOne第一个参数是:映射文件中statement的id，等于namespace + "." + statement的id；
-			//selectOne第二个参数是:指定和映射文件中statement设置的paramterType类型的参数；
-			//selectOne的结果就是与映射文件中statement设置的resultType类型的对象；
+
+			// 通过SqlSession对象操作数据库
+			// selectOne第一个参数是:映射文件中statement的id，等于namespace + "." +
+			// statement的id；
+			// selectOne第二个参数是:指定和映射文件中statement设置的paramterType类型的参数；
+			// selectOne的结果就是与映射文件中statement设置的resultType类型的对象；
 			User user = session.selectOne("test.findUserById", 1);
-			
-			//查询操作不需要事物的支持
-			
+
+			// 查询操作不需要事物的支持
+
 			System.out.println(user.getUsername());
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally{
-			//释放SqlSession会话资源
+		} finally {
+			// 释放SqlSession会话资源
 			session.close();
 		}
 	}
 
+	//根据username，查询，可能返回多条记录
+	@Test
+	public void findUserByNameTest() throws IOException {
+
+		SqlSession session = null;
+		try {
+			InputStream inputSream = Resources.getResourceAsStream("mybatis_config.xml");
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputSream);
+			session = factory.openSession();
+			List<User> users = session.selectList("test.findUserByName", "小明");
+
+			for (User user : users) {
+				System.out.println(user.getUsername());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 释放SqlSession会话资源
+			session.close();
+		}
+	}
 }
--- mybatis_shop/src/main/java/mybatis/shop/jdbcdao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package mybatis.shop.jdbcdao;
-
-import mybaitsi.shop.model.User;
-
-public interface IUserDao {
-	
-	public User load(int userId);
-}
