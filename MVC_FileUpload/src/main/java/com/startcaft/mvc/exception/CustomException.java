7494ddb19121a98dc9caddf27f7ac2198606a3f1
startcaft <51418996@qq.com>
2016-01-30 21:24:04 +0800

    配置CommonsMultipartResolver，多部分解析器来。 HTML form标签添加enctype=”multipart/form-data”属性。 SpringMVC Controller的方法中使用MultipartFile类型的参数来接受上传的文件。 然后将接受到的内存中的文件保存到本地指定目录中。

---
+++ MVC_FileUpload/src/main/java/com/startcaft/mvc/exception/CustomException.java
@@ -0,0 +1,27 @@
+package com.startcaft.mvc.exception;
+
+/**
+ * 自定义异常
+ */
+public class CustomException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+	
+	public CustomException(String message) {
+		super(message);
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ MVC_FileUpload/src/main/java/com/startcaft/mvc/vo/ItemListVo.java
@@ -0,0 +1,15 @@
+package com.startcaft.mvc.vo;
+
+import java.util.List;
+
+public class ItemListVo {
+	
+	private List<ItemsVo> items;
+
+	public List<ItemsVo> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemsVo> items) {
+		this.items = items;
+	}
+}
